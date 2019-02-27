295b7fa7da146ddfd44c323cd445f7216e570a80
soekmego <soekmen.goekhan@gmail.com>
2019-02-27 20:33:40 +0100

    Added Knife and functionality

--- Weapon_knife.gd
+++ Weapon_knife.gd
@@ -1,9 +1,9 @@
 extends Spatial
 
-const DAMAGE = 4
+const DAMAGE = 40
 
-const IDLE_ANIM_NAME = "Rifle_idle"
-const FIRE_ANIM_NAME = "Rifle_fire"
+const IDLE_ANIM_NAME = "Knife_idle"
+const FIRE_ANIM_NAME = "Knife_fire"
 
 var is_weapon_enabled = false
 
@@ -13,17 +13,15 @@ func _ready():
 	pass
 
 func fire_weapon():
-	var ray = $Ray_Cast
-	ray.force_raycast_update()
+	var area = $Area
+	var bodies = area.get_overlapping_bodies()
 	
-	if ray.is_colliding():
-		var body = ray.get_collider()
-		
+	for body in bodies:
 		if body == player_node:
-			pass
-		elif body.has_method("bullet_hit"):
-			body.bullet_hit(DAMAGE, ray.global_transform)
-
+			continue
+		
+		if body.has_method("bullet_hit"):
+			body.bullet_hit(DAMAGE, area.global_transform)
 
 func equip_weapon():
 	if player_node.animation_manager.current_state == IDLE_ANIM_NAME:
@@ -31,13 +29,14 @@ func equip_weapon():
 		return true
 	
 	if player_node.animation_manager.current_state == "Idle_unarmed":
-		player_node.animation_manager.set_animation("Rifle_equip")
-		return false
+		player_node.animation_manager.set_animation("Knife_equip")
+	
+	return false
 
 func unequip_weapon():
+
 	if player_node.animation_manager.current_state == IDLE_ANIM_NAME:
-		if player_node.animation_manager.current_state != "Rifle_unequip":
-			player_node.animation_manager.set_animation("Rifle_unequip")
+		player_node.animation_manager.set_animation("Knife_unequip")
 	
 	if player_node.animation_manager.current_state == "Idle_unarmed":
 		is_weapon_enabled = false
